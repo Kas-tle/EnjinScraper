d9638c1835e0a819916e0ae64dbea0274991c795
PC <pc@kvalvik.no>
2023-04-28 22:24:24 +0200

    Support multiple SiteAuths in notifier

--- src/util/notifier.ts
+++ src/util/notifier.ts
@@ -8,7 +8,7 @@ import { addExitListeners, removeExitListeners } from "./exit";
 import { enjinRequest } from "./request";
 import { Messages } from "../interfaces/messages";
 
-export async function startNotifier(database: Database, domain: string, apiKey: string, siteAuth: SiteAuth, messageSubject: string, messageBody: string) {
+export async function startNotifier(database: Database, domain: string, apiKey: string, siteAuths: SiteAuth[], messageSubject: string, messageBody: string) {
     statusMessage(MessageType.Info, 'Running in notifier mode...');
     statusMessage(MessageType.Info, 'The users module will be immediately scraped, and then the program will begin sending messages to each user found');
     statusMessage(MessageType.Critical, 'NOTE: It is highly reccomended that you do not use this mode with your primary account, as it very well could be banned by Enjin');
@@ -32,6 +32,24 @@ export async function startNotifier(database: Database, domain: string, apiKey: 
     });
 
     let userCount = [0];
+    const rateLimits: { 
+        [authId: number]: Date | undefined
+    } = Object.fromEntries(siteAuths.map((_, i) => [i, undefined]));
+
+    const findNextAuth = () => {
+        const authIds = Object.keys(rateLimits).map(id => parseInt(id));
+        
+        // Return the first auth without a rate-limit
+        const authsWithoutRateLimit = authIds.filter(id => !rateLimits[id]);
+        if (authsWithoutRateLimit.length > 0) {
+            return authsWithoutRateLimit[0];
+        }
+        
+        // Return the auth with the closest rate-limit to now
+        const authsWithRateLimit = authIds.filter(id => rateLimits[id]);
+        const nextRateLimit = Math.min(...authsWithRateLimit.map(id => rateLimits[id]!.getTime()));
+        return authsWithRateLimit.find(id => rateLimits[id]!.getTime() === nextRateLimit)!;
+    }
 
     if (fileExists('./target/recovery/notifier_progress.json')) {
         statusMessage(MessageType.Info, 'Recovering notifier progress from previous session...')
@@ -46,6 +64,21 @@ export async function startNotifier(database: Database, domain: string, apiKey: 
     statusMessage(MessageType.Info, `Sending messages to ${totalUsers} users... Estimated time: ${(totalUsers - userCount[0]) * 21 / 3600} hours`);
 
     for (let i = userCount[0]; i < totalUsers; i++) {
+        
+        // Select the next auth with the lowest rate or no rate-limit
+        const authId = findNextAuth();
+        statusMessage(MessageType.Process, `Selecting Auth ${authId} with rate-limit '${rateLimits[authId] ? rateLimits[authId]!.getTime() : 'none'}'...`);
+
+        // Sleep if the auth has a rate-limit
+        const delay = rateLimits[authId] ? Math.max(rateLimits[authId]!.getTime() - Date.now(), 0) : 0;
+        if (delay > 0) {
+            statusMessage(MessageType.Process, `Waiting ${delay / 1000} seconds before sending next message...`);
+            await new Promise<void>(resolve => setTimeout(() => resolve(), delay));
+        }
+        
+        // Select the site auth with this auth id
+        const siteAuth = siteAuths[authId % siteAuths.length];
+
         userCount = [i]
         const pmRequest = await enjinRequest<Messages.SendMessage> ({
             recipients: [users[i].user_id],
@@ -78,21 +111,26 @@ export async function startNotifier(database: Database, domain: string, apiKey: 
                         statusMessage(MessageType.Error, `Did not expect unit ${unit}... Exiting...`);
                         process.kill(process.pid, 'SIGINT');
                     }
-                    statusMessage(MessageType.Process, `Waiting ${timeInMilliseconds}ms (${time} ${unit}) before trying ${users[i].user_id} ${users[i].username} again...`);
-                    await new Promise((resolve) => setTimeout(resolve, timeInMilliseconds + 1000));
+                    statusMessage(MessageType.Process, `Auth ${authId} rate-limited for ${timeInMilliseconds}ms (${time} ${unit}). Trying ${users[i].user_id} ${users[i].username} again...`);
+
+                    // Add the rate-limit to the auth
+                    rateLimits[authId] = new Date(Date.now() + timeInMilliseconds);
 
                     // Retry the user.
                     i--;
+                    
                     continue;
                 } else {
                     process.kill(process.pid, 'SIGINT');
                 }
             }
-            await new Promise((resolve) => setTimeout(resolve, 21000));
+            // Set the rate-limit to 21 seconds for this auth as to avoid spamming the API
+            rateLimits[authId] = new Date(Date.now() + 21000);
             continue;
         }
 
-        await new Promise((resolve) => setTimeout(resolve, 21000));
+        // Set the rate-limit to 21 seconds for auth as to avoid spamming the API
+        rateLimits[authId] = new Date(Date.now() + 21000);
         statusMessage(MessageType.Process, `Sent message to ${users[i].user_id} ${users[i].username} [(++${userCount[0]}/${totalUsers})]`);
     }
 
